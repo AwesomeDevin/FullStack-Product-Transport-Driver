f1d8cd428ef3988b981ae1c474fbff798ec65874
wb.dengwen <wb.dengwen@mesg.corp.netease.com>
2017-10-22 20:46:19 +0800

    司机版前端页面

--- front/src/main.js
+++ front/src/main.js
@@ -1,15 +1,35 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vonic from 'vonic/src/index.js'
 
-Vue.config.productionTip = false
+// Page Components
+import Index from './components/Index.vue'
+import About from './components/About.vue'
+import Intro from './components/Intro.vue'
+import Login from './components/Login.vue'
+import Welcome from './components/Welcome.vue'
+import Register from './components/Register.vue'
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+
+
+
+
+// Routes
+const routes = [
+  { path: '/', component: Index },
+  { path: '/about', component: About },
+  { path: '/intro', component: Intro,
+    children:[
+        {path:'welcome', component:Welcome},
+        {path:'login', component:Login},
+        {path:'register', component:Register},
+    ]
+ },
+  // { path: '/login', component: Login },
+   {path:'*', redirect:'/intro/welcome'}  //404
+
+
+]
+
+Vue.use(Vonic.app, {
+  routes: routes
 })
